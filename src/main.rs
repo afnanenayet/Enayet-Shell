0c0a6b82d1301f84b13e8d8e52578dccdce8d348
Afnan Enayet <afnan.enayet@gmail.com>
2017-06-17 00:38:57 -0400

    Added skeleton functions for shell UI

--- src/main.rs
+++ src/main.rs
@@ -17,7 +17,7 @@
 /// * 1 - error reading config file
 ///
 
-use std::io;
+use std::io::{self, BufRead, Write};
 
 // Program wide constants
 const VERSION: &'static str = env!("CARGO_PKG_VERSION");
@@ -25,7 +25,19 @@ const VERSION: &'static str = env!("CARGO_PKG_VERSION");
 // Entry point for shell
 // Initializees the shell, then loops program, waiting for commands
 fn main() {
+    // Initialize shell
     init_shell();
+    
+    let mut valid_cmd = true;
+    
+    // Loop until user wants to exit
+    while valid_cmd {
+        valid_cmd = shell_loop();
+    }
+    
+    // Returns an exit code to the shell
+    let return_code = shell_exit();
+    std::process::exit(return_code);
 }
 
 // Initialize shell, using config file provided from arguments (if any)
@@ -33,5 +45,41 @@ fn main() {
 // default does not exist, create a default config file with default 
 // paths
 fn init_shell() {
-    println!("Enayet Shell, v{}", VERSION);
+    println!("Enayet Shell | v{}", VERSION);
 }
+
+// Captures input from stdin and executes commands from 
+fn shell_loop() -> bool {
+    let exit_code = "exit";
+    let shell_prompt = ">";
+
+    print_shell_prompt(shell_prompt);
+    // Read input from stdin
+    let stdin = io::stdin();
+
+    // "elegant syntax"
+    // TODO unsafe, change this
+    let input = stdin.lock().lines().next().unwrap().unwrap();
+
+    // Exit if necessary 
+    if input != exit_code {
+        println!("cmd: {}", input);
+        return true;
+    } else {
+        return false;
+    }
+}
+
+// Cleans up and exits the shell
+fn shell_exit() -> i32 {
+    println!("Thank you for using the Enayet shell");
+    return 0;
+}
+
+// Prints shell prompt to STDOUT
+fn print_shell_prompt(prompt: &str) {
+    println!("(working directory)");
+    print!("{} ", prompt);
+    io::stdout().flush().unwrap();
+}
+
