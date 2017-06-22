40ca71dec24375525ae2b27f59d05185763061e6
Afnan Enayet <afnan.enayet@gmail.com>
2017-06-22 11:52:26 -0400

    Wrote individual tests to ensure that R/W operations for config don't panic

--- src/parser/config.rs
+++ src/parser/config.rs
@@ -5,7 +5,7 @@
 
 use std::io;
 use std::fs::File;
-use std::path::{Path, PathBuf};
+use std::path::{Path};
 use std::io::{Write, BufReader, BufRead};
 
 // Loads a list of include paths from the config file. The function takes 
@@ -14,14 +14,14 @@ use std::io::{Write, BufReader, BufRead};
 // to load the default config file. If that cannot be loaded, the function 
 // will create a default config file in the default path
 pub fn load_paths_from_config(config_path: Option<&str>) -> Vec<String> {
-    let DEFAULT_CONFIG_PATH = "~/.ensh_config";
+    let default_config_path = "~/.ensh_config";
 
     // Try to load given path, or use default if no string was supplied
-    let config_path = config_path.unwrap_or(DEFAULT_CONFIG_PATH);
+    let config_path = config_path.unwrap_or(default_config_path);
 
     // See if the file can be opened
     let file = File::open(config_path);
-    let default_exists = Path::new(DEFAULT_CONFIG_PATH).exists();
+    let default_exists = Path::new(default_config_path).exists();
 
     // Open the default file if the supplied location fails
     let file = match file {
@@ -30,10 +30,10 @@ pub fn load_paths_from_config(config_path: Option<&str>) -> Vec<String> {
             // Try to read from default config
             // Create a new default config if necessary
             if default_exists {
-                File::open(DEFAULT_CONFIG_PATH).unwrap()
+                File::open(default_config_path).unwrap()
             } else {
-                create_default_config(DEFAULT_CONFIG_PATH).unwrap();
-                File::open(DEFAULT_CONFIG_PATH).unwrap()
+                create_default_config(default_config_path).unwrap();
+                File::open(default_config_path).unwrap()
             }
         }
     };
@@ -43,7 +43,7 @@ pub fn load_paths_from_config(config_path: Option<&str>) -> Vec<String> {
 
     // Read config file line by line, adding paths that exist
     for line in reader.lines().enumerate() {
-        if Path::new(DEFAULT_CONFIG_PATH).exists() {
+        if Path::new(default_config_path).exists() {
             let line_result = line.1;
             result.push(line_result.unwrap().to_string());
         }
@@ -54,14 +54,15 @@ pub fn load_paths_from_config(config_path: Option<&str>) -> Vec<String> {
 // Creates the default configuration file in the default location. Will 
 // panic if for some reason cannot write default config to path
 fn create_default_config(file_path: &str) -> Result<(), io::Error> {
-    let mut file = File::create(file_path).unwrap();
+    let mut file = File::create(file_path)?;
 
-    let DEFAULT_PATHS = vec! [
+    let default_paths = vec! [
         "hello",
+        "world",
     ];
 
-    for line in DEFAULT_PATHS {
-        file.write(line.as_bytes()).unwrap(); 
+    for line in default_paths {
+        file.write(line.as_bytes())?; 
     }
     file.flush()?;
     Ok(())
@@ -70,5 +71,29 @@ fn create_default_config(file_path: &str) -> Result<(), io::Error> {
 // unit tests
 #[cfg(test)]
 mod tests {
+    use super::*;
+
+    // Test if the config file is able to write to the filesystem
+    #[test]
+    fn test_write_config_fs() {
+        create_default_config("~/test_config").expect("Unable to write file");
+    }
+
+
+    // Test if the shell can read a config file from the filesystem
+    #[test]
+    fn test_read_config_fs() {
+        load_paths_from_config(Some("/Users/aenayet/test_config"));
+    }
+
+    // Tests that the functions are able to both read and write to a file 
+    // correctly with the proper output
+    #[test]
+    fn verify_read_write_config() {
+        let config_path = "./test_config_rw";
+        create_default_config(config_path).expect("Unable to write file");
+        let paths = load_paths_from_config(Some(config_path));
+        assert_eq!(paths.len(), 1);
+    }
 }
 
