38b33a3a9fa1705c3fed6d8274d3a50490e5d3eb
Afnan Enayet <afnan.enayet@gmail.com>
2017-07-27 03:08:29 -0400

    Reinstated all tests

--- src/lib.rs
+++ src/lib.rs
@@ -107,6 +107,6 @@ mod tests {
     // Test that shell can be initialized properly
     #[test]
     fn test_init_shell() {
-        // let mut shell = init_shell(None)?;
+        let shell = init_shell(None);
     }
 }
