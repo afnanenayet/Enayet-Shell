27717d03d971e1fca495e327eb20822fc9dacbf2
Afnan Enayet <afnan.enayet@gmail.com>
2017-08-24 18:43:53 -0400

    Added build script to create compressed exe

---
+++ build_release.sh
@@ -0,0 +1,16 @@
+# build_release.sh    Afnan Enayet
+# Builds the release version of the shell with cargo and compresses it with 
+# gzexe
+
+#!/usr/bin/bash
+
+# Build the executable
+cargo build --release
+
+# Compress the executable
+gzexe ./target/release/ensh 
+
+# Retain original executable and rename the compressed executable 
+mv ./target/release/ensh ./target/release/ensh_compressed
+mv ./target/release/ensh~ ./target/release/ensh
+
