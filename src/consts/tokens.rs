35dc5caa9bba322c519cedd5e834fb392777f744
Afnan Enayet <afnan.enayet@gmail.com>
2018-04-06 14:54:19 -0400

    Add continuation token for multiline commands on the command line

--- src/consts/tokens.rs
+++ src/consts/tokens.rs
@@ -12,9 +12,13 @@ pub const OP_REDIR: &'static str = ">";
 /// Token delineating command separation
 pub const OP_SEP: &'static str = ";";
 
+/// Token delineating command continuation after newline
+pub const OP_CONTINUE = &'static str = "\\";
+
 /// An array of all valid shell language tokens
 pub const OP_TOKENS: &'static [&'static str] = &[
     OP_PIPE,
     OP_REDIR,
     OP_SEP,
+    OP_CONTINUE,
 ];
