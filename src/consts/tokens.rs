e321ef38fcc84c55dfd73707ff239a8bf27396ee
Afnan Enayet <afnan.enayet@gmail.com>
2018-05-01 16:24:51 -0400

    * change tokens to be chars instead of full strings

--- src/consts/tokens.rs
+++ src/consts/tokens.rs
@@ -4,16 +4,16 @@
 /// `ensh` scripting language.
 
 /// Token delineating pipe operation
-pub const OP_PIPE: &'static str = "|";
+pub const OP_PIPE: char = '|';
 
 /// Token delineating file redirection
-pub const OP_REDIR: &'static str = ">";
+pub const OP_REDIR: char = '>';
 
 /// Token delineating command separation
-pub const OP_SEP: &'static str = ";";
+pub const OP_SEP: char = ';';
 
 /// Token delineating command continuation after newline
-pub const OP_CONTINUE: &'static str = "\\";
+pub const OP_CONTINUE: char = '\\';
 
 /// An array of all valid shell language tokens
-pub const OP_TOKENS: &'static [&'static str] = &[OP_PIPE, OP_REDIR, OP_SEP, OP_CONTINUE];
+pub const OP_TOKENS: &'static [char] = &[OP_PIPE, OP_REDIR, OP_SEP, OP_CONTINUE];
